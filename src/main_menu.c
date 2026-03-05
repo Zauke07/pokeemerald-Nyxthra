895091072badea0cacf6cbeafdb01e59fbe75b88
Dennis Zauke <dennis.zauke@icloud.com>
2026-03-05 23:26:09 +0100

    Core: Implement style system, all-region starters, and main menu updates

--- src/main_menu.c
+++ src/main_menu.c
@@ -2017,7 +2017,9 @@ static void MainMenu_FormatSavegameBadges(void)
     u8 str[0x20];
     u8 badgeCount = 0;
     u32 i;
-    for (i = FLAG_BADGE01_GET; i < FLAG_BADGE01_GET + NUM_BADGES; i++) if (FlagGet(i)) badgeCount++;
+    for (i = 0; i < NUM_BADGES; i++)
+        if (FlagGet(gBadgeFlags[i]))
+            badgeCount++;
     StringExpandPlaceholders(gStringVar4, gText_ContinueMenuBadges);
     AddTextPrinterParameterized3(2, FONT_NORMAL, 0x6C, 33, sTextColor_MenuInfo, TEXT_SKIP_DRAW, gStringVar4);
     ConvertIntToDecimalStringN(str, badgeCount, STR_CONV_MODE_LEADING_ZEROS, 2);
